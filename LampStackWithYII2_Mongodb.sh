2eaea207d5ba03dcfd8b511fb0f8e8d31a252110
eroll231 <57205903+eroll231@users.noreply.github.com>
2020-01-30 13:42:36 +0800

    Update LampStackWithYII2_Mongodb.sh

--- LampStackWithYII2_Mongodb.sh
+++ LampStackWithYII2_Mongodb.sh
@@ -16,12 +16,54 @@ export LC_ALL=en_US.UTF-8
 
 echo "############################################################"
 echo "############################################################"
-echo "######################|---------------|#####################"
-echo "######################| Install MySQL |#####################"
-echo "######################|---------------|####################"
+echo "######################|-----------------|###################"
+echo "######################| Install MongoDB |###################"
+echo "######################|-----------------|###################"
 echo "############################################################"
 echo "############################################################"
-sudo apt install mysql-server mysql-client -y
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt update
+sudo apt install mongodb -y
+sudo systemctl enable mongodb
+sudo systemctl start mongodb
+sudo systemctl stop mongodb
+sudo systemctl restart mongodb
+
+echo "############################################################"
+echo "############################################################"
+echo "##################|-----------------------|#################"
+echo "##################| Creating User MongoDB |#################"
+echo "##################|-----------------------|#################"
+echo "############################################################"
+echo "############################################################"
+
+mongo
+
+use admin
+
+db.createUser(
+  {
+    user: "admin",
+    pwd: "hive1234",
+    roles: [ { role: "userAdminAnyDatabase", db: "admin" } ]
+  }
+)
+
+exit
+
+mongod --port 27017 -u "admin" -p "hive1234" --authenticationDatabase "admin"
+
+use test
+
+db.createUser(
+  {
+    user: "user",
+    pwd: "hive1234",
+    roles: [ { role: "readWrite", db: "test" } ]
+  }
+)
+exit
+ech0 "Success.";
 
 
 echo "############################################################"
@@ -84,7 +126,7 @@ echo "###################|--------------------|###################"
 echo "############################################################"
 echo "############################################################"
 sudo apt-get install php7.0-bcmath php7.0-cli php7.0-common php7.0-curl php7.0-dev php7.0-gd php7.0-json php7.0-json php7.0-mcrypt php7.0-mysql php7.0-mbstring php7.0-xml php7.0-zip -y
-
+sudo apt install php-pear php-mongodb -y
 
 echo "############################################################"
 echo "############################################################"
