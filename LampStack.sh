3cc1d58ecce6b787613446771e784045edaf2688
eroll231 <57205903+eroll231@users.noreply.github.com>
2020-01-30 11:10:54 +0800

    Update LampStack.sh

--- LampStack.sh
+++ LampStack.sh
@@ -62,6 +62,7 @@ echo "############################################################"
 echo "############################################################"
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+unlink composer-setup.php
 
 
 echo "############################################################"
