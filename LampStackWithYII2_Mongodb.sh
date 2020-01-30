f9f5fa0c113eda62d605a9e4f522b860d72d41b1
eroll231 <57205903+eroll231@users.noreply.github.com>
2020-01-30 14:05:40 +0800

    Update LampStackWithYII2_Mongodb.sh

--- LampStackWithYII2_Mongodb.sh
+++ LampStackWithYII2_Mongodb.sh
@@ -31,6 +31,23 @@ sudo systemctl restart mongodb
 
 echo "############################################################"
 echo "############################################################"
+echo "######################|-----------------|###################"
+echo "######################| Install MongoDB |###################"
+echo "######################|-----------------|###################"
+echo "############################################################"
+echo "############################################################"
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt update
+sudo apt install mongodb -y
+sudo systemctl enable mongodb
+sudo systemctl start mongodb
+sudo systemctl stop mongodb
+sudo systemctl restart mongodb
+
+
+
+echo "############################################################"
+echo "############################################################"
 echo "##################|-----------------------|#################"
 echo "##################| Creating User MongoDB |#################"
 echo "##################|-----------------------|#################"
@@ -49,10 +66,6 @@ db.createUser(
   }
 )
 
-exit
-
-mongod --port 27017 -u "admin" -p "hive1234" --authenticationDatabase "admin"
-
 use test
 
 db.createUser(
@@ -62,8 +75,8 @@ db.createUser(
     roles: [ { role: "readWrite", db: "test" } ]
   }
 )
-exit
-ech0 "Success.";
+trap sig_handler 15
+echo "Success.";
 
 
 echo "############################################################"
