99284d775fa83fe7a208a6e559808cf73969fd9f
eroll231 <57205903+eroll231@users.noreply.github.com>
2020-02-13 16:21:16 +0800

    Update Laravel.sh

--- Laravel.sh
+++ Laravel.sh
@@ -172,7 +172,7 @@ echo '
         ServerAdmin webmaster@localhost
         DocumentRoot /var/www/html/blog/public
         
-        <Directory "/var/www/html/advanced/backend/web">
+        <Directory "var/www/html/blog/public">
             Options +FollowSymLinks -Indexes
             RewriteEngine On
 
