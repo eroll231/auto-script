a2543a32f9d3a421ca6c4046037096595fefab88
eroll231 <57205903+eroll231@users.noreply.github.com>
2020-02-13 16:30:21 +0800

    Update Laravel.sh

--- Laravel.sh
+++ Laravel.sh
@@ -47,7 +47,6 @@ echo "###################|--------------------|###################"
 echo "############################################################"
 echo "############################################################"
 sudo apt-get --yes --force-yes install php7.2-bcmath php7.2-cli php7.2-common php7.2-curl php7.2-dev php7.2-gd php7.2-json php7.2-mysql php7.2-mbstring php7.2-xml php7.2-zip
-sudo apt-get --yes --force-yes install php7.4-bcmath php7.4-cli php7.4-common php7.4-curl php7.4-dev php7.4-gd php7.4-json php7.4-mysql php7.4-mbstring php7.4-xml php7.4-zip
 
 
 
@@ -117,6 +116,7 @@ echo "####################|Installing LARAVEL5.6|#################"
 echo "####################|---------------------|#################"
 echo "############################################################"
 echo "############################################################"
+sudo apt-get --yes --force-yes install php7.4-bcmath php7.4-cli php7.4-common php7.4-curl php7.4-dev php7.4-gd php7.4-json php7.4-mysql php7.4-mbstring php7.4-xml php7.4-zip
 sudo composer global require "laravel/installer"
 
 
