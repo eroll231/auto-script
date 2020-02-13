0712f927c8b96a53f30f7b87e475ef65736e3001
eroll231 <57205903+eroll231@users.noreply.github.com>
2020-02-13 16:26:49 +0800

    Update Laravel.sh

--- Laravel.sh
+++ Laravel.sh
@@ -94,7 +94,7 @@ echo "####################|---------------------|#################"
 echo "############################################################"
 echo "############################################################"
 curl -sS https://getcomposer.org/installer -o composer-setup.php
-sudo php7.2 composer-setup.php --install-dir=/usr/local/bin --filename=composer
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 unlink composer-setup.php
 
 
