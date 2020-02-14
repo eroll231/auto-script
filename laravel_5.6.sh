ca1f09a3bc590f415bb762f55f28667b5206a1c7
eroll231 <57205903+eroll231@users.noreply.github.com>
2020-02-14 11:33:34 +0800

    Update laravel_5.6.sh

--- laravel_5.6.sh
+++ laravel_5.6.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 
-echo "###################################[ 1 ]###################################";
+echo "###################################[ 1 ]###################################"
 
 
 sudo add-apt-repository ppa:ondrej/php
@@ -11,7 +11,7 @@ sudo apt-get update
 sudo apt-get install apache2 libapache2-mod-php7.2 php7.2 php7.2-bcmath php7.2-cli php7.2-common php7.2-curl php7.2-dev php7.2-gd php7.2-json php7.2-mysql php7.2-mbstring php7.2-xml php7.2-zip
 
 
-echo "###################################[ 2 ]###################################";
+echo "###################################[ 2 ]###################################"
 echo "mysql-server mysql-server/root_password password hive1234" | sudo debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password hive1234" | sudo debconf-set-selections
 sudo apt install mysql-server mysql-client -y
@@ -21,7 +21,7 @@ sudo apt install mysql-server mysql-client -y
 sudo apt install mysql-server mysql-client -y
 
 
-echo "###################################[ 3 ]###################################";
+echo "###################################[ 3 ]###################################"
 cd /tmp
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
@@ -30,7 +30,7 @@ sudo mv composer.phar /usr/local/bin/composer
 
 
 
-echo "###################################[ 4 ]###################################";
+echo "###################################[ 4 ]###################################"
 sudo fallocate -l 1G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
@@ -41,7 +41,7 @@ exit
 
 
 
-echo "###################################[ 5 ]###################################";
+echo "###################################[ 5 ]###################################"
 sudo composer global require laravel/installer
 cd /var/www/html
 # sudo composer create-project laravel/laravel laravelProject --prefer-dist
@@ -51,20 +51,20 @@ sudo composer create-project --prefer-dist laravel/laravel laravelProject "5.6.*
 
 
 
-echo "###################################[ 6 ]###################################";
+echo "###################################[ 6 ]###################################"
 sudo chgrp -R www-data /var/www/html/laravelProject/
 sudo chmod -R 775 /var/www/html/laravelProject/
 
 
 
 
-echo "###################################[ 7 ]###################################";
+echo "###################################[ 7 ]###################################"
 cd /etc/apache2/sites-available
 sudo touch laravel.conf
 
 
 
-echo "###################################[ 8 ]###################################";
+echo "###################################[ 8 ]###################################"
 sudo su
 echo '
 <VirtualHost *:80>
@@ -93,7 +93,7 @@ sudo a2enmod rewrite
 sudo service apache2 restart
 
 
-echo "###################################[ 9 ]###################################";
+echo "###################################[ 9 ]###################################"
 clear
 ifconfig
 echo "Done, copy your IP and paste to url";
