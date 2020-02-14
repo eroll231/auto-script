49f183561d7f2d2b39b7a2e4e659c3a6fc0b8cb1
eroll231 <57205903+eroll231@users.noreply.github.com>
2020-02-14 11:42:21 +0800

    Update laravel_5.6.sh

--- laravel_5.6.sh
+++ laravel_5.6.sh
@@ -11,10 +11,10 @@ sudo apt-get update
 sudo apt-get install apache2 libapache2-mod-php7.2 php7.2 php7.2-bcmath php7.2-cli php7.2-common php7.2-curl php7.2-dev php7.2-gd php7.2-json php7.2-mysql php7.2-mbstring php7.2-xml php7.2-zip
 
 
-echo "###################################[ 2 ]###################################"
-echo "mysql-server mysql-server/root_password password hive1234" | sudo debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password hive1234" | sudo debconf-set-selections
-sudo apt install mysql-server mysql-client -y
+# echo "###################################[ 2 ]###################################"
+# echo "mysql-server mysql-server/root_password password hive1234" | sudo debconf-set-selections
+# echo "mysql-server mysql-server/root_password_again password hive1234" | sudo debconf-set-selections
+# sudo apt install mysql-server mysql-client -y
 
 
 
