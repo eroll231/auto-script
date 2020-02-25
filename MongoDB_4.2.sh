e04680f17cd19b72f893ed58f027ee197c0dd585
eroll231 <57205903+eroll231@users.noreply.github.com>
2020-02-25 09:34:25 +0800

    Update and rename mongo_4.2.sh to MongoDB_4.2.sh

---
+++ MongoDB_4.2.sh
@@ -0,0 +1,23 @@
+echo "############################################################"
+echo "############################################################"
+echo "######################|-----------------|###################"
+echo "######################| Install MongoDB |###################"
+echo "######################|-----------------|###################"
+echo "############################################################"
+echo "############################################################"
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 4B7C549A058F8B6B
+
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
+
+sudo apt update
+
+sudo apt install mongodb-org -y
+
+sudo systemctl enable mongod
+sudo systemctl start mongod 
+sudo systemctl stop mongod
+sudo systemctl restart mongod 
+
+clear
+echo "Done !"
