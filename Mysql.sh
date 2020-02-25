cff21c7ea315a6432f659f385bbab303107ba46c
eroll231 <57205903+eroll231@users.noreply.github.com>
2020-02-25 09:35:31 +0800

    Update Mysql.sh

--- Mysql.sh
+++ Mysql.sh
@@ -10,4 +10,7 @@ echo "mysql-server mysql-server/root_password password root" | sudo debconf-set-
 echo "mysql-server mysql-server/root_password_again password root" | sudo debconf-set-selections
 sudo apt install mysql-server mysql-client -y
 
+clear
 echo "Now install a module, base on your php version";
+echo "Done !"
+
