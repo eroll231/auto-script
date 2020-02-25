68e8e2a72867c843465817d8b1a98d8382e07d70
eroll231 <57205903+eroll231@users.noreply.github.com>
2020-02-25 09:38:04 +0800

    Update Mysql.sh

--- Mysql.sh
+++ Mysql.sh
@@ -6,13 +6,13 @@ echo "######################| Install MySQL |#####################"
 echo "######################|---------------|####################"
 echo "############################################################"
 echo "############################################################"
-echo "mysql-server mysql-server/root_password password root" | sudo debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password root" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password password hive1234" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password hive1234" | sudo debconf-set-selections
 sudo apt install mysql-server mysql-client -y
 
 clear
 echo "Now install a module, base on your php version";
 echo "Done !"
 echo "usr: root"
-echo "pwd: password"
+echo "pwd: hive1234"
 
